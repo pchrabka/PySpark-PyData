a3342ff0de10eddc32d882338554bd04bbb754b7
p.chrabka <p.chrabka@marketinvoice.com>
2019-11-23 19:09:55 +0000

    Added config file

--- app/movies.py
+++ app/movies.py
@@ -1,12 +1,16 @@
+import json
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, expr
 
-spark = SparkSession.builder.appName("Movies").getOrCreate()
+with open("config.json", "r") as config_file:
+    config = json.load(config_file)
+
+spark = SparkSession.builder.appName(config.get("app_name")).getOrCreate()
 
 raw_df = (
     spark.read.format("csv")
     .option("header", "true")
-    .load("data/ml-latest-small/movies.csv")
+    .load(f"{config.get('source_data_path')}/movies.csv")
 )
 
 transformed_df = raw_df.select(
@@ -15,4 +19,6 @@ transformed_df = raw_df.select(
     col("title").substr(-5, 4).alias("year"),
 )
 
-transformed_df.write.mode("overwrite").parquet("data/output/movies")
+transformed_df.write.mode("overwrite").parquet(
+    f"{config.get('output_data_path')}/movies"
+)
